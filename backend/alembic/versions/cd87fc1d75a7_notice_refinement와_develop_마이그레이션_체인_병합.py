6ece40e8f89265e835d8280693d13a98c56a3c6f
Kim Dong Min <kkimniu@gmail.com>
2026-07-07 11:12:19 +0900

    chore: 병합 리비전 파일 lint/포맷 정리
    alembic merge로 자동 생성된 파일에 unused import(op, sa)가 있어
    ruff/black 기준에 맞게 정리.

--- backend/alembic/versions/cd87fc1d75a7_notice_refinement와_develop_마이그레이션_체인_병합.py
+++ backend/alembic/versions/cd87fc1d75a7_notice_refinement와_develop_마이그레이션_체인_병합.py
@@ -5,15 +5,12 @@ Revises: 0f54ab544cb2, f42111cd547f
 Create Date: 2026-07-07 11:05:07.399723
 
 """
+
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
-
 # revision identifiers, used by Alembic.
-revision: str = 'cd87fc1d75a7'
-down_revision: Union[str, Sequence[str], None] = ('0f54ab544cb2', 'f42111cd547f')
+revision: str = "cd87fc1d75a7"
+down_revision: Union[str, Sequence[str], None] = ("0f54ab544cb2", "f42111cd547f")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
